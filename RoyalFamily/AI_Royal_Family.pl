340c6122ba3a083b310a9e2f1a4b8cd32a9ddb71
jivko25 <jivkou@yahoo.com>
2021-04-20 19:26:32 +0300

    RoyalFamilyProjectAdded

---
+++ RoyalFamily/AI_Royal_Family.pl
@@ -0,0 +1,300 @@
+male(albertOfSaxeCoburgGotha).
+birth(albertOfSaxeCoburgGotha, 1819).
+death(albertOfSaxeCoburgGotha, 1861).
+
+female(victoria).
+birth(victoria, 1819).
+death(1901, 1861).
+
+female(victoriaAdelaideMaryLouisa).
+birth(victoriaAdelaideMaryLouisa, 1840).
+death(victoriaAdelaideMaryLouisa, 1901).
+parent(victoriaAdelaideMaryLouisa, albertOfSaxeCoburgGotha).
+parent(victoriaAdelaideMaryLouisa, victoria).
+
+female(alice).
+birth(alice,1843).
+death(alice, 1878).
+parent(alice, albertOfSaxeCoburgGotha).
+parent(alice, victoria).
+
+male(alfred).
+birth(alfred, 1844).
+death(alfred, 1900).
+parent(alfred, albertOfSaxeCoburgGotha).
+parent(alfred, victoria).
+
+female(helena).
+birth(helena, 1846).
+death(helena, 1923).
+parent(helena, albertOfSaxeCoburgGotha).
+parent(helena, victoria).
+
+male(edwardVII).
+birth(edwardVII, 1841).
+death(edwardVII, 1910).
+parent(victoriaAdelaideMaryLouisa, albertOfSaxeCoburgGotha).
+parent(victoriaAdelaideMaryLouisa, victoria).
+
+female(alexandra).
+birth(alexandra, 1844).
+death(alexandra, 1925).
+
+female(louisaCarolineAlberta).
+birth(louisaCarolineAlberta, 1848).
+death(louisaCarolineAlberta, 1939).
+parent(louisaCarolineAlberta, albertOfSaxeCoburgGotha).
+parent(louisaCarolineAlberta, victoria).
+
+male(arthur).
+birth(arthur, 1850).
+death(arthur, 1942).
+parent(arthur, albertOfSaxeCoburgGotha).
+parent(arthur, victoria).
+
+male(leopold).
+birth(leopold, 1853).
+death(leopold, 1884).
+parent(leopold, albertOfSaxeCoburgGotha).
+parent(leopold, victoria).
+
+female(bewtrice).
+birth(bewtrice, 1857).
+death(bewtrice, 1944).
+parent(bewtrice, albertOfSaxeCoburgGotha).
+parent(bewtrice, victoria).
+
+male(georgeV).
+birth(georgeV, 1865).
+death(georgeV, 1936).
+parent(georgeV, edwardVII).
+parent(georgeV, alexandra).
+
+female(maryOfTeck).
+birth(moud, 1867).
+death(moud, 1953).
+
+male(albert).
+birth(albert, 1864).
+death(albert, 1931).
+parent(albert, edwardVII).
+parent(albert, alexandra).
+
+female(louise).
+birth(louise, 1867).
+death(louise, 1931).
+parent(louise, edwardVII).
+parent(louise, alexandra).
+
+female(victoriaAlexandraOlgaMary).
+birth(victoriaAlexandraOlgaMary, 1868).
+death(victoriaAlexandraOlgaMary, 1935).
+parent(victoriaAlexandraOlgaMary, edwardVII).
+parent(victoriaAlexandraOlgaMary, alexandra).
+
+female(moud).
+birth(moud, 1869).
+death(moud, 1938).
+parent(moud, edwardVII).
+parent(moud, alexandra).
+
+male(alexanderJohn).
+birth(alexanderJohn, 1871).
+death(alexanderJohn, 1871).
+parent(alexanderJohn, edwardVII).
+parent(alexanderJohn, alexandra).
+
+male(edwardVIII).
+birth(edwardVIII, 1894).
+death(edwardVIII, 1972).
+parent(edwardVIII, georgeV).
+parent(edwardVIII, maryOfTeck).
+
+male(georgeVI).
+birth(georgeVI, 1895).
+death(georgeVI, 1952).
+parent(georgeVI, georgeV).
+parent(georgeVI, maryOfTeck).
+
+female(elizabethBowesLyon).
+birth(elizabethBowesLyon, 1900).
+death(elizabethBowesLyon, 2002).
+
+female(mary).
+birth(mary, 1897).
+death(mary, 1955).
+parent(mary, georgeV).
+parent(mary, maryOfTeck).
+
+male(henry).
+birth(henry, 1900).
+death(henry, 1974).
+parent(henry, georgeV).
+parent(henry, maryOfTeck).
+
+male(george).
+birth(george, 1902).
+death(george, 1942).
+parent(george, georgeV).
+parent(george, maryOfTeck).
+
+male(john).
+birth(john, 1905).
+death(john, 1919).
+parent(john, georgeV).
+parent(john, maryOfTeck).
+
+female(elizabethII).
+birth(elizabethII, 1925).
+parent(elizabethII, georgeVI).
+parent(elizabethII, elizabethBowesLyon).
+
+male(philip).
+birth(philip, 1921).
+death(philip, 2021).
+
+female(margaret).
+birth(margaret, 1930).
+death(margaret, 2002).
+parent(margaret, georgeVI).
+parent(margaret, elizabethBowesLyon).
+
+male(charles).
+birth(charles, 1948).
+parent(charles, philip).
+parent(charles, elizabethII).
+
+female(diana).
+birth(diana, 1961).
+death(diana, 1997).
+
+female(camilla).
+birth(camilla, 1947).
+
+female(anne).
+birth(anne, 1950).
+parent(anne, philip).
+parent(anne, elizabethII).
+
+male(markPhillips).
+birth(markPhillips, 1948).
+
+male(andrew).
+birth(andrew, 1960).
+parent(andrew, philip).
+parent(andrew, elizabethII).
+
+female(sarah).
+birth(sarah, 1959).
+
+male(edward).
+birth(edward, 1964).
+parent(edward, philip).
+parent(edward, elizabethII).
+
+female(sophie).
+birth(sophie, 1965).
+
+male(william).
+birth(william, 1982).
+parent(william, charles).
+parent(william, diana).
+
+female(catherine).
+birth(sophie, 1982).
+
+
+male(harry).
+birth(harry, 1984).
+parent(harry, charles).
+parent(harry, diana).
+
+female(meghan).
+birth(sophie, 1981).
+
+male(peter).
+birth(peter, 1977).
+parent(peter, markPhillips).
+parent(peter, anne).
+
+female(autumn).
+birth(autumn, 1978).
+
+female(zara).
+birth(zara, 1981).
+parent(zara, markPhillips).
+parent(zara, anne).
+
+male(mike).
+birth(mike, 1978).
+
+female(beatrice).
+birth(beatrice, 1988).
+parent(beatrice, andrew).
+parent(beatrice, sarah).
+
+female(eugenie).
+birth(eugenie, 1990).
+parent(eugenie, andrew).
+parent(eugenie, sarah).
+
+female(ladyLouise).
+birth(ladyLouise, 2003).
+parent(ladyLouise, edward).
+parent(ladyLouise, sophie).
+
+male(james).
+birth(james, 2007).
+parent(james, edward).
+parent(james, sophie).
+
+male(georgeLouise).
+birth(princeGeorge, 2013).
+parent(princeGeorge, william).
+parent(princeGeorge, catherine).
+
+female(charlotte).
+birth(charlotte, 2015).
+parent(charlotte, william).
+parent(charlotte, catherine).
+
+male(louis).
+birth(louis, 2018).
+parent(louis, william).
+parent(louis, catherine).
+
+male(archie).
+birth(louis, 2019).
+parent(louis, harry).
+parent(louis, meghan).
+
+female(savannah).
+birth(savannah, 2010).
+parent(savannah, peter).
+parent(savannah, autumn).
+
+female(mia).
+birth(mia, 2014).
+parent(mia, mike).
+parent(mia, zara).
+
+female(lena).
+birth(lena, 2018).
+parent(lena, mike).
+parent(lena, zara).
+
+father(A, B):-male(B), parent(A, B).
+mother(A, B):-female(B), parent(A, B).
+son(A, B):-male(B), parent(B, A).
+daughter(A, B):-female(B), parent(B, A).
+grandfather(A, B):-male(B), parent(A, C), parent(C, B).
+grandmother(A, B):-female(B), parent(A, C), parent(C, B).
+sister(A,B) :- female(A),parent(B,C),parent(A,C), A \= B.
+brother(A,B) :- male(A),parent(B,C),parent(A,C), A \= B.
+aunt(A,B) :- female(A),sister(A,C),(mother(B, C) ; father(B, C)).
+uncle(A,B) :- male(A),brother(A,C),(mother(B, C) ; father(B, C)).
+cousin(A,B) :- (uncle(C , A) ; aunt(C, A)),(father(B,C) ; mother(B, C)).
+
+older(X, Y):- birth(X, A), birth(Y, B), A<B.
+younger(X, Y):- birth(X, A), birth(Y, B), A>B.
+
